a0267bc59416bd1b5fb555ab3b69a9b5a06251ae
Daniel Deluz <60910376+DeluzD@users.noreply.github.com>
2022-11-23 15:25:35 -0600

    Revert "Merge branch 'main' of https://github.com/brettaseltine/comp3020MusicPlayer"
    This reverts commit 7a3617f5d42853cc78a6193fde6ba1171a47029e, reversing
    changes made to 1c75410d6ff2d99807f9d19d8fc435a463f76cec.

--- js/settings.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener("pointermove", (e)=>{
-    const { currentTarget: el, clientX: x, clientY: y } = e;
-    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
-    el.style.setProperty('--posX',  x-l-w/2);
-    el.style.setProperty('--posY',  y-t-h/2);
-  })
