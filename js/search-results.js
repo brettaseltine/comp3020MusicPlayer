9e965293c565a33d9cbf5e5c4eef0cfc341f94ee
Daniel Deluz <60910376+DeluzD@users.noreply.github.com>
2022-11-25 14:33:11 -0600

    Added forward and backward buttons

--- js/search-results.js
+++ js/search-results.js
@@ -3,3 +3,7 @@ const searchParam = document.getElementById("searchQuery");
 function showResults(){
     document.write(searchParam);
 }
+
+function initializeHandlers(){
+    songs = document.getElementById(son)
+}
