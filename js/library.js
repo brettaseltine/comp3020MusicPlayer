5d3ea64085773b165ff152706d76412f15edae89
vijdan-anjum <m-vijdan@hotmail.com>
2022-11-28 03:33:55 -0600

    updated library.css

--- js/library.js
+++ js/library.js
@@ -1,40 +1,42 @@
-function shiftLeft() {
-  const boxes = document.querySelectorAll(".box");
-  const tmpNode = boxes[0];
-  boxes[0].className = "box move-out-from-left";
+function toggleLeft() {
+  const cardArray = document.querySelectorAll(".card");
+  const tempCard_Node = cardArray[0];
+  cardArray[0].className = "card move-out-from-left";
   setTimeout(function () {
-    if (boxes.length > 5) {
-      tmpNode.classList.add("box--hide");
-      boxes[5].className = "box move-to-position5-from-left";
-    }
-    boxes[1].className = "box move-to-position1-from-left";
-    boxes[2].className = "box move-to-position2-from-left";
-    boxes[3].className = "box move-to-position3-from-left";
-    boxes[4].className = "box move-to-position4-from-left";
-    boxes[0].remove();
-    document.querySelector(".cards__container").appendChild(tmpNode);
+      if (cardArray.length > 5) {
+          tempCard_Node.classList.add("card--hide");
+          cardArray[5].className = "card move-to-position5-from-left";
+      }
+      cardArray[1].className = "card move-to-position1-from-left";
+      cardArray[2].className = "card move-to-position2-from-left";
+      cardArray[3].className = "card move-to-position3-from-left";
+      cardArray[4].className = "card move-to-position4-from-left";
+      cardArray[0].remove();
+      document.querySelector(".card-list").appendChild(tempCard_Node);
   }, 500);
 }
-function shiftRight() {
-  const boxes = document.querySelectorAll(".box");
-  boxes[4].className = "box move-out-from-right";
+function toggleRight() {
+  const cardArray = document.querySelectorAll(".card");
+  cardArray[4].className = "card move-out-from-right";
   setTimeout(function () {
-    const noOfCards = boxes.length;
-    if (noOfCards > 4) {
-      boxes[4].className = "box box--hide";
-    }
-    const tmpNode = boxes[noOfCards - 1];
-    tmpNode.classList.remove("box--hide");
-    boxes[noOfCards - 1].remove();
-    let parentObj = document.querySelector(".cards__container");
-    parentObj.insertBefore(tmpNode, parentObj.firstChild);
-    tmpNode.className = "box move-to-position1-from-right";
-    boxes[0].className = "box move-to-position2-from-right";
-    boxes[1].className = "box move-to-position3-from-right";
-    boxes[2].className = "box move-to-position4-from-right";
-    boxes[3].className = "box move-to-position5-from-right";
+      const numCards = cardArray.length;
+      if (numCards > 4) {
+          cardArray[4].className = "card card--hide";
+      }
+      const tempCard_Node = cardArray[numCards - 1];
+      tempCard_Node.classList.remove("card--hide");
+      cardArray[numCards - 1].remove();
+      let cardList_Obj = document.querySelector(".card-list");
+      cardList_Obj.insertBefore(tempCard_Node, cardList_Obj.firstChild);
+      tempCard_Node.className = "card move-to-position1-from-right";
+      cardArray[0].className = "card move-to-position2-from-right";
+      cardArray[1].className = "card move-to-position3-from-right";
+      cardArray[2].className = "card move-to-position4-from-right";
+      cardArray[3].className = "card move-to-position5-from-right";
   }, 500);
 }
+
+
 /** ---------- */
 var dateContainers = document.getElementsByClassName("date-container");
 var detailContainers = document.getElementsByClassName("row");
@@ -81,3 +83,4 @@ function sticky() {
 window.onscroll = function () {
   sticky();
 }
+
